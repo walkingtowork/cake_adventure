4f4b30c7fdab1d0dd051ac6369236349bae4be6e
Alex Lin <alexander.lin.kremer@gmail.com>
2013-06-18 01:46:43 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CakeAdventure::Application.config.secret_token = '39722331eee5af7c8f56a260db42ceb48e1d9750fb20d7e6e67cadb36a547c46d0413005b1e59a55ab9ec87e8677154574456b600e3941861d6547db98c2253a'
