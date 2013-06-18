fe8b7f8644de47bc928df1b54e1642b2e3c54625
Isaac Elias <nicecurelom@gmail.com>
2013-06-18 08:31:54 -0700

    Finally got this freaking fb auth working.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adventure::Application.config.secret_token = '46d7d8bf82d8d099428612783727258098f09b8d61e525a1659331b0699c0391a8ce3b1352aa09a9e2448d6d717e85a50f5b3c73dc2a5fccb2415c2cf7240f04'
